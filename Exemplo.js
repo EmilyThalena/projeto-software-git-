5451806e4bdc9b8f25f300d147bf9236d752ab23
EmilyThalena <emythale27@gmail.com>
2024-12-05 20:18:51 -0300

    adicionar arquivo de exemplo

---
+++ Exemplo.js
@@ -0,0 +1,9 @@
+ //Para organizar o projeto de forma eficiente, utilizamos a **interface do usuário (frontend)** e a **lógica do backend**.como isso pode ser feito:
+ 
+//**Branches para Funcionalidades do Projeto main ou master:**
+
+//- Branch principal onde o código finalizado será integrado.
+//feature/interface-usuario:
+
+//- Objetivo: Desenvolver a interface do usuário (HTML, CSS, layouts).
+//Atividades: Criar formulários, páginas e fluxos de interação.
